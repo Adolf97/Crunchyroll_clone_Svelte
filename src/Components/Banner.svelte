09c64bdd8dce064c19cd51fc84e98a954b5799d8
Adolfo Ramirez <ramirez.desanc@gmail.com>
2023-05-12 14:48:38 -0600

    Agregué un nuevo componente: Banner. Y añadí una nueva imagen a la carpeta de assets

---
+++ src/Components/Banner.svelte
@@ -0,0 +1,19 @@
+<script>
+    import Banner from '../lib/assets/aot_banner.jpg'
+    import '../Styles/Banner.css'
+</script>
+
+<div class="banner__container">
+    <div class="image__container">
+        <img src={Banner} alt="">
+        <button class="btn__watchNow">WATCH NOW</button>
+    </div>
+    <div class="anime_season__container">
+        <div class="anime_season__item">Shingeky No Kyojin: Final Season</div>
+        <div class="anime_season__item">KONOSUBA - An Explosion on This...</div>
+        <div class="anime_season__item">MASHLE: MAGIC AND MUSCLES</div>
+        <div class="anime_season__item">Dr.STONE: Season 3 New Episode</div>
+        <div class="anime_season__item">KamiKatsu: Working for God in a Godless World</div>
+        <div class="anime_season__item">I Got a Cheat Skill in Another World</div>
+    </div>
+</div>
